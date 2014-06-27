548d272bd54796def512d0feb964eb83afe02e65
mcfisto <daniel@man.eu.com>
2014-06-27 20:21:39 +0200

    Cleanup

--- app.js
+++ app.js
@@ -1,60 +1,59 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require ('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+//var user = require('./routes/user');
+//var webApp = require('./routes/app');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.methodOverride());
+app.use(express.bodyParser({uploadDir: '/tmp'}));
+app.use(express.limit('32mb'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// session support
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: 'HjN8*&&ahj[::9io'
+}));
+
+// CSRF protection middleware
+app.use(express.csrf());
+app.configure(function() {
+	app.use(function(req, res, next) {
+		res.locals.token = req.csrfToken();
+		next();
+	});
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+
+app.use(app.router);
+
+app.get('/', routes.index);
+//app.get('/app/new', webApp.startOrder);
+//app.post('/app/step1Process', webApp.step1Process);
+//app.get('/app/step2Prepare', webApp.step2Prepare);
+//app.get('/app/text', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+/*
+ * GET home page.
+ */
 
-module.exports = router;
+exports.index = function(req, res){
+  res.render('index', { title: 'Reef Monitor' });
+};
