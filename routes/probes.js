bf52d4456ff5f6bdf75a4e9bdd0553570ca3d98d
mand <mand@PHWMAN7.ad.cmhb.cz>
2014-09-01 22:56:05 +0200

    New features

--- routes/probes.js
+++ routes/probes.js
@@ -14,17 +14,19 @@ exports.list = function(req, res){
 };
 
 exports.newManual = function(req, res){	
-	res.render('probe-manual', { "title": 'Reef Monitor :: Nový typ ručního měření'});
+	res.render('probe', { "title": 'Reef Monitor :: Nový typ ručního měření'
+		, "frmType": 'MANUAL'});
 };
 
 exports.save = function(req, res){
+	// cancel???
+	
 	req.assert('frmName', 'Název je povinný.').notEmpty();
-	req.assert('frmUnit', 'Jednotka je povinný.').notEmpty();
-	req.assert('frmMinVal', 'Minimum OK je povinné.').notEmpty();
-	req.assert('frmMaxVal', 'Maximum OK je povinné.').notEmpty();
-	req.assert('frmMinWVal', 'Minimum varování je povinné.').notEmpty();
-	req.assert('frmMaxWVal', 'Maximum varování je povinné.').notEmpty();
+	req.assert('frmUnit', 'Jednotka je povinná.').notEmpty();
 	req.assert('frmMinVal', 'Minimum OK musí být číslo.').isFloat();
+	req.assert('frmMaxVal', 'Maximum OK musí být číslo.').isFloat();
+	req.assert('frmMinWVal', 'Minimum varování musí být číslo.').isFloat();
+	req.assert('frmMaxWVal', 'Maximum varování musí být číslo.').isFloat();
 
 	var errors = req.validationErrors();		
 
@@ -33,6 +35,8 @@ exports.save = function(req, res){
 
 	// write values
 	res.locals.frmName = probe.name = req.param('frmName');
+	res.locals.frmType = probe.type = req.param('frmType');
+	res.locals.frmCode = probe.code = req.param('frmCode');
 	res.locals.frmUnit = probe.unit = req.param('frmUnit');
 	res.locals.frmMinVal = probe.min = req.param('frmMinVal');
 	res.locals.frmMaxVal = probe.max = req.param('frmMaxVal');
@@ -44,20 +48,37 @@ exports.save = function(req, res){
 	if (errors){
 		// print errors
 		console.log(errors);
+		res.locals.errors = errors;
 		exports.newManual(req, res);
 	} else {			
 		probe.isInService = 1;
 		probe.isSystem = 0;
-		probe.type = 'MANUAL';
+		
+		// convert
+		req.sanitize('frmMinVal').toFloat();
+		req.sanitize('frmMaxVal').toFloat();
+		req.sanitize('frmMinWVal').toFloat();
+		req.sanitize('frmMaxWVal').toFloat();
+		
+		// write values
+		probe.min = req.param('frmMinVal');
+		probe.max = req.param('frmMaxVal');
+		probe.minW = req.param('frmMinWVal');
+		probe.maxW = req.param('frmMaxWVal');
+		
 		// save
 		console.log(probe);
-		res.redirect('/probes');
+		probeDao.saveProbe(probe, function(probe){
+			res.redirect('/probes');
+		});		
 	}
 };
 
 exports.newAuto = function(req, res){
 	probeDao.readProbes(function(probes){
-		res.render('probe-auto', { "title": 'Reef Monitor', "probes": probes});
+		res.render('probe', { "title": 'Reef Monitor :: Nové elektronické čidlo'
+			, "frmType": 'AUTO'});
+		// type: 1Wire, Atlas
 		// probeDao.finalize(); TODO
 	});
   
