e0aeca803f357d421ea19e23af9fe9ecc1dd4d97
mcfisto <daniel@man.eu.com>
2014-09-05 23:16:30 +0200

    New structure

--- test.js
+++ test.js
@@ -3,8 +3,10 @@
  */
 
 var Dao = require("./lib/dao/ProbeDao")();
+var ParamsDao = require("./lib/dao/ParamsDao")();
 
 var dao = new Dao();
+var paramsDao = new ParamsDao();
 
 dao.readProbe('PH1', function(probe){
 	console.log(probe.lastValue);
@@ -12,6 +14,10 @@ dao.readProbe('PH1', function(probe){
 
 dao.readProbes(function(probes){
 	console.log(probes[0].code);
+});
+
+paramsDao.getAndIncreaseTick(function(tick){
+	console.log(tick);
 });
 
 dao.finalize();
