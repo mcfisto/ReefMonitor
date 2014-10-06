790bef6aecd21ac7e1ba9f59bc38a68ab7f0d1d9
mcfisto <daniel@man.eu.com>
2014-10-06 22:32:32 +0200

    i2c

--- thirdparty/Adafruit_PWM_Driver/SetValue.py
+++ thirdparty/Adafruit_PWM_Driver/SetValue.py
@@ -5,8 +5,8 @@ import sys
 from Adafruit_PWM_Servo_Driver import PWM
 
 # read values
-channel = sys.argv[1];
-value = sys.argv[2];
+channel = int(sys.argv[1]);
+value = int(sys.argv[2]);
 
 # instantiate
 pwm = PWM(0x40, debug=True)
